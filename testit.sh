66f0e0b7cec24a076bcd1190779d4bc664eb7986
qidu <teric@outlook.com>
2023-11-03 13:22:32 +0800

    fix test, add data

--- testit.sh
+++ testit.sh
@@ -1,7 +1,10 @@
+# create test data file
+dd if=/dev/zero of=/tmp/filename bs=1M count=2
+
 # test send from client
 WORKMODE=recv go run ../kcpserver.go
-./kcp_test_nofec 127.0.0.1 send
+./qtp_test 127.0.0.1 send
 
 # test send from serv
 WORKMODE=send go run ../kcpserver.go
-./kcp_test_nofec 127.0.0.1 recv
+./qtp_test 127.0.0.1 recv
